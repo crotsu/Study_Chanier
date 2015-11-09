262b5d5be1f2721cd742e791d2aeecf6370997b7
crotsu <crotsu@gmail.com>
2015-11-09 15:08:28 +0900

    mnistを読み込んで表示する

---
+++ mnist/dispDataFromMnist.py
@@ -0,0 +1,38 @@
+# coding: utf-8
+import numpy as np
+import six
+import matplotlib.pyplot as plt
+
+# mnistの画像を表示する
+def draw_digit(data):
+    size = 28
+    plt.figure(figsize=(2.5, 3))
+
+    X, Y = np.meshgrid(range(size),range(size))
+    Z = data.reshape(size,size)   # convert from vector to 28x28 matrix
+    Z = Z[::-1,:]             # flip vertical
+    plt.xlim(0,27)
+    plt.ylim(0,27)
+    plt.pcolor(X, Y, Z)
+    plt.gray()
+    plt.tick_params(labelbottom="off")
+    plt.tick_params(labelleft="off")
+
+    plt.show()
+
+# すでにmnist.pklになっているデータを読み込む
+with open('mnist.pkl', 'rb') as mnist_pickle:
+  mnist = six.moves.cPickle.load(mnist_pickle)
+
+mnist['data'] = mnist['data'].astype(np.float32)
+mnist['data'] /= 255
+mnist['target'] = mnist['target'].astype(np.int32)
+
+N = 60000
+x_train, x_test = np.split(mnist['data'],   [N])
+y_train, y_test = np.split(mnist['target'], [N])
+N_test = y_test.size
+
+# 表示して確認する
+#draw_digit(x_train[12345])
+#print(y_train[12345])
