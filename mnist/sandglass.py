17b194f48d6364c77f011c5bc7169a4bfde574f9
Shinichi Oeda <crotsu@gmail.com>
2015-11-09 20:56:38 +0900

    砂時計型DNNを作成

--- mnist/sandglass.py
+++ mnist/sandglass.py
@@ -29,25 +29,23 @@ N_test = y_test.size
 
 # パラメータ
 batchsize = 100
-n_epoch = 2
+n_epoch = 20
 
 # モデルを作る
-# 砂時計型にする
 model = chainer.FunctionSet(l1=F.Linear(784, 256),
                             l2=F.Linear(256, 2),
                             l3=F.Linear(2, 256),
                             l4=F.Linear(256, 784))
 
 # 前向き計算
-def forward(x_data, y_data, train=True):
-  # データの型をchainer.Variableに変換する
-  x, t = chainer.Variable(x_data), chainer.Variable(y_data)
-
+def forward(x_batch, y_batch, train=True):
+  x, t = chainer.Variable(x_batch), chainer.Variable(y_batch)
   h1 = F.dropout(F.relu(model.l1(x)),  train=train)
   h2 = F.dropout(F.sigmoid(model.l2(h1)), train=train)
   h3 = F.dropout(F.relu(model.l3(h2)), train=train)
+  y = F.dropout(F.relu(model.l4(h3)), train=train)
 
-  return F.mean_squared_error(h3, t), F.accuracy(h3, t)
+  return F.mean_squared_error(y, t)
 
 # 最適化を設定
 optimizer = optimizers.Adam()
@@ -59,19 +57,15 @@ for epoch in six.moves.range(1, n_epoch + 1):
 
     # training
     perm = np.random.permutation(N)
-    sum_accuracy = 0
     sum_loss = 0
     for i in six.moves.range(0, N, batchsize):
       x_batch = np.asarray(x_train[perm[i:i + batchsize]])
 
       optimizer.zero_grads()
-      loss, acc = forward(x_batch, x_batch)
+      loss = forward(x_batch, x_batch)
       loss.backward()
       optimizer.update()
 
       sum_loss += float(loss.data) * len(x_batch)
-      sum_accuracy += float(acc.data) * len(x_batch)
 
-    print('train mean loss={}, accuracy={}'.format(
-        sum_loss / N, sum_accuracy / N))
-
+    print('train mean loss={}'.format(sum_loss / N))
